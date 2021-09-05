50683d92cf85079207abcc8445653f808a4d96e5
jhackmeyer <jhackmeyer@ucla.edu>
2021-09-04 17:53:49 -0700

    Adding filtered table function

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,22 @@ function buildTable(data) {
         });
     });
 }
+
+// Create function for button click
+function handleClick () {
+    // Look for datetime id and store value in date
+    let date = d3.select("#datetime").property("value");
+    // Save default filtered data in new variable
+    let filteredData = tableData;
+    // Filter, show rows of data only if date matches 
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Build table with filtered data
+    buildTable(filteredData);
+};
+// Attach event to listen for form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build table when page loads
+buildTable(tableData);
